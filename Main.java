f5d2fc27cf0e581253f2edfbaad8f3e1c05c52bd
Eric <hedgreneric@gmail.com>
2024-11-10 08:02:22 -0600

    Committing changes

--- Main.java
+++ Main.java
@@ -2,8 +2,13 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        String[] topics = {"tennis", "grand slam"};
-        WikiCrawler w = new WikiCrawler("/wiki/Tennis", topics, 50, "WikiTennisGraph.txt");
+        String[] topics = {"ball", "goal"};
+
+        String outputFile = "Graph.txt";
+        for (int i = 0; i < topics.length; i++) {
+            outputFile = topics[i] + "-" + outputFile;
+        }
+        WikiCrawler w = new WikiCrawler("/wiki/Tennis", topics, 50, outputFile);
         w.crawl();
     }
 }
